108d5fb936026dac1d864b5ddab6c491b84d460f
jameno <jasonmeno@msn.com>
2017-12-04 02:27:46 -0500

    Path Finding, Level Creation, and Bullet Tracing

--- main.js
+++ main.js
@@ -6,19 +6,39 @@
 ***************************************/
 
 var highscores = [];
-
+//initialize arrays outside of document.ready for memory synchronization
+var bulletLoc = [];
+var playerBullet = [];
+var playerBulletLoc = [];
+var Enemy = [];
+var bulletAngle = [];
+var enemyAngle = [];
+var playerBulletAngle = [];
+var playerAngle= [];
+var Bounds = [];
+var EnemyBounds = [];
+var enemyBulletTrace = [];
+var enemiesFiring = [];
+var playerX;
+var playerY;
+var currentLevel = [[],[],[],[]];
+var campaignMode = false;
+var survivalMode = [[225,200],[],[],[20,20]];
+var level1 = [[5,5],[550,20,100,375],[50,50,100,100,200,50,100,100,350,50,100,100,50,200,100,100,200,200,100,100,350,200,100,100],[550,370]];
 
 $(document).ready(function(){
 
 	$(".SurvivalButton").click(function () {
 		$(".MainMenu").hide();
-        $(".DeathScreen").hide();
+    $(".DeathScreen").hide();
 		$(".CreditScreen").hide();
 		$(".HighScoresScreen").hide();
-        $("#canvas").show();
+    $("#canvas").show();
+		currentLevel = survivalMode;
+		createLevel();
 		offsetDY = accel.getAY();
-        offsetDX = accel.getAX();
-        start = setInterval(draw, 33);
+    offsetDX = accel.getAX();
+    start = setInterval(draw, 33);
 		var clock = setInterval(clock, 100);
     draw();
 
@@ -26,13 +46,16 @@ $(document).ready(function(){
 
 		$(".CampaignButton").click(function () {
 			$(".MainMenu").hide();
-					$(".DeathScreen").hide();
+			$(".DeathScreen").hide();
 			$(".CreditScreen").hide();
 			$(".HighScoresScreen").hide();
-					$("#canvas").show();
+			$("#canvas").show();
+			currentLevel = level1;
+			campaignMode = true;
+			createLevel();
 			offsetDY = accel.getAY();
-					offsetDX = accel.getAX();
-					start = setInterval(draw, 33);
+			offsetDX = accel.getAX();
+			start = setInterval(draw, 33);
 			var clock = setInterval(clock, 100);
 			draw();
 
@@ -68,7 +91,7 @@ $(document).ready(function(){
 		$(".MainMenu").show();
         $(".DeathScreen").hide();
 				$(".HighScoresScreen").hide();
-        $("#canvas").hide();
+        $(".canvas").hide();
 		$(".CreditScreen").hide();
 
 	});
@@ -83,13 +106,20 @@ $(document).ready(function(){
 	dY = 0;
 	xVal = 0;
 	yVal = 0;
-	maxAccel = 15;
+	maxAccel = 5;
+	firstPathCheck = true;
+	updateCounter = 1;
 
 
-	var bulletLoc = [];
-	var playerBullet = [];
-	var playerBulletLoc = [];
-	var Enemy = [];
+	//currentLevel uses the following variables
+	//[[playerStartX, playerStartY],[enemy1X,enemy1Y....enemyNX,enemyNY], [bounds1StartX, bounds1StartY, bounds1Width, bounds1Height,....boundsN][coinStartX, coinStartY]]
+	//currentLevel = [[5,5],[],[50,50,100,100,200,50,100,100,50,200,100,100,200,200,100,100],[25,25]];
+	//currentLevel = [[5,5],[],[],[400,50]];
+
+
+	//bounds = createBoundaries(220-18,200-18,100+18,100+18);
+
+
 	var coinX, coinY, enemyX, enemyY;
 	var changeDY, changeDX;
 	var offsetDX, offsetDY;
@@ -109,7 +139,10 @@ $(document).ready(function(){
 	var coinAudio = document.getElementById("coin");
 	var bulletAudio = document.getElementById("bullet");
 
-	newCoin();
+	createLevel();
+	//newCoin();
+
+
 
 	var drawing = document.getElementById("canvas");
 	document.body.appendChild(drawing);
@@ -124,7 +157,6 @@ $(document).ready(function(){
 		mouseX = e.offsetX;
  		mouseY = e.offsetY;
 		playerBulletFire();
-
 
 	}
 	function checkKeyDown(e) {
@@ -214,16 +246,29 @@ $(document).ready(function(){
 		playerX += dX;
 		playerY += dY;
 
-		checkWall();
+		checkBoundaries();
 
 		//check for gold coin collision
 		if(checkCoinCollision(playerX, playerY)){
-			newCoin();
+			if(!campaignMode){
+				score++;
+				newCoin();
+			}
+			else{
+				//Go to next level
+				score++;
+				endGame();
+			}
 			score++;
 			newEnemy();
 		}
 
-		updatePlayerBullet();
+		if(playerBullet.length > 0){
+			updatePlayerBullet();
+			checkPlayerBulletHit();
+			playerBulletWallCollision();
+		}
+
 		updateBullet();
 		checkPlayerBulletHit();
 		updateEnemy();
@@ -259,9 +304,17 @@ $(document).ready(function(){
 		con.lineWidth = 5;
 		con.strokeRect(0, 0, 600, 400);
 
+		//Draw Boundaries
 		con.fillStyle = "black";
 		con.fillRect(playerX,playerY,18,18);
 
+		for(b=0; b<=currentLevel[2].length;b+=4){
+			con.fillRect(currentLevel[2][b],currentLevel[2][b+1],currentLevel[2][b+2],currentLevel[2][b+3]);
+		}
+
+
+
+		con.fillRect(playerX,playerY,18,18);
 		con.fillStyle = "gold"
 		con.fillRect(coinX,coinY,10,10);
 		con.fillStyle = "yellow";
@@ -275,6 +328,19 @@ $(document).ready(function(){
 
 		for(i = 0; i <= Enemy.length-1; i+=2){
 
+			if(enemyBulletTrace.length > 0){
+
+				for(j=0; j<enemyBulletTrace[i].length-2; j+=2){
+					alpha = 2/j;
+					fillString = "rgba(255,0,0,"
+					con.fillStyle = fillString.concat(String(alpha),")");
+					//con.fillStyle = "rgba(255,0,0,0.7)";
+					con.fillRect(enemyBulletTrace[i][j], enemyBulletTrace[i][j+1],5,5);
+
+				}
+			}
+
+
 			con.fillStyle = "blue";
 			con.fillRect(bulletLoc[i],bulletLoc[i+1],5,5);
 
@@ -285,7 +351,6 @@ $(document).ready(function(){
 		for (i = 0; i < playerBulletLoc.length; i+=2){
 
 			con.fillStyle = "black";
-
 			con.fillRect(playerBulletLoc[i],playerBulletLoc[i+1],5,5);
 
 		}
@@ -405,7 +470,7 @@ $(document).ready(function(){
 	  	enemyY = Math.random() * (375);
 
 	  	//check for collisions
-	  	if(checkCoinCollision(enemyX, enemyY) || checkEnemyCollision(enemyX,enemyY)  ){
+	  	if(checkCoinCollision(enemyX, enemyY) || checkEnemyCollision(enemyX,enemyY) ){
 				newEnemy();
 
 	  	}
@@ -477,7 +542,9 @@ $(document).ready(function(){
 	  	return false;
 	  }//end checkBulletHit()
 
-	  function checkWall(){
+	  function checkBoundaries(){
+
+		//check player collision with canvas limits
 
 	  	if(playerX+18 >= 597){
 	  		playerX = 579;
@@ -491,7 +558,51 @@ $(document).ready(function(){
 	  	if(playerY <= 3){
 	  		playerY = 3;
 	  	}
-	  }//end checkWall()
+
+			//check player collision with boundaries
+
+			for(b=0; b<=currentLevel[2].length; b+=4){
+				if(playerX+18 >= currentLevel[2][b] && playerX <= currentLevel[2][b]+currentLevel[2][b+2] && playerY+18 >= currentLevel[2][b+1] && playerY <= currentLevel[2][b+1]+currentLevel[2][b+3]){
+
+					if(playerX-3 < currentLevel[2][b] && playerY+15 >= currentLevel[2][b+1] && playerY+3 < currentLevel[2][b+1]+currentLevel[2][b+3]){
+						playerX = currentLevel[2][b]-18;
+					}
+					if(playerX+15 > currentLevel[2][b]+currentLevel[2][b+2] && playerY+15 > currentLevel[2][b+1] && playerY+3 < currentLevel[2][b+1]+currentLevel[2][b+3]){
+						playerX = currentLevel[2][b]+currentLevel[2][b+2];
+					}
+					if(playerY-3 < currentLevel[2][b+1] && playerX+15 > currentLevel[2][b] && playerX+3 < currentLevel[2][b]+currentLevel[2][b+2]){
+						playerY = currentLevel[2][b+1]-18;
+					}
+					if(playerY+15 > currentLevel[2][b+1]+currentLevel[2][b+3] && playerX+15 > currentLevel[2][b] && playerX+3 < currentLevel[2][b]+currentLevel[2][b+2]){
+						playerY = currentLevel[2][b+1]+currentLevel[2][b+3];
+					}
+				}
+			}
+
+			//check player collision with boundaries
+
+			for(b=0; b<=Enemy.length/2; b+=2){
+				if(Enemy[b]+18 >= currentLevel[2][b] && Enemy[b] <= currentLevel[2][b]+currentLevel[2][b+2] && Enemy[b+1]+18 >= currentLevel[2][b+1] && Enemy[b+1] <= currentLevel[2][b+1]+currentLevel[2][b+3]){
+
+					if(Enemy[b]-3 < currentLevel[2][b] && Enemy[b+1]+15 >= currentLevel[2][b+1] && Enemy[b+1]+3 < currentLevel[2][b+1]+currentLevel[2][b+3]){
+						Enemy[b] = currentLevel[2][b]-18;
+					}
+					if(Enemy[b]+15 > currentLevel[2][b]+currentLevel[2][b+2] && Enemy[b+1]+15 > currentLevel[2][b+1] && Enemy[b+1]+3 < currentLevel[2][b+1]+currentLevel[2][b+3]){
+						Enemy[b] = currentLevel[2][b]+currentLevel[2][b+2];
+					}
+					if(Enemy[b+1]-3 < currentLevel[2][b+1] && Enemy[b]+15 > currentLevel[2][b] && Enemy[b]+3 < currentLevel[2][b]+currentLevel[2][b+2]){
+						Enemy[b+1] = currentLevel[2][b+1]-18;
+					}
+					if(Enemy[b+1]+15 > currentLevel[2][b+1]+currentLevel[2][b+3] && Enemy[b]+15 > currentLevel[2][b] && Enemy[b]+3 < currentLevel[2][b]+currentLevel[2][b+2]){
+						Enemy[b+1] = currentLevel[2][b+1]+currentLevel[2][b+3];
+					}
+				}
+			}
+
+
+			//if(playerX )
+	  }//end checkBoundaries()
+
 
 	  function checkBulletWallCollision(){
 
@@ -501,8 +612,38 @@ $(document).ready(function(){
 		  	if(bulletLoc[i] >= 597 || bulletLoc[i] <= 0 || bulletLoc[i+1] >= 397 || bulletLoc[i+1] <= 0){
 		  		newBullet(Enemy[i],Enemy[i+1],i);
 		  	}//end if
+				for(b=0; b<=currentLevel[2].length; b+=4){
 
-		 }//end for
+						if(bulletLoc[i]+5 >= currentLevel[2][b] && bulletLoc[i] <= currentLevel[2][b]+currentLevel[2][b+2] && bulletLoc[i+1]+5 >= currentLevel[2][b+1] && bulletLoc[i+1] <= currentLevel[2][b+1]+currentLevel[2][b+3]){
+
+
+							path = enemyPath(Enemy[i], Enemy[i+1], playerX, playerY);
+							collisionPoint = pathObsticalCollisionCheck(path);
+/*
+							//don't make a new bullet if player cannot be seen
+							if(collisionPoint){
+								bulletLoc.splice(i,2);
+								//bulletLoc.splice(Enemy[i],Enemy[i+1]);
+								bulletAngle.splice(i,2);
+								enemyBulletTrace.splice(0,2,0,0);
+								enemiesFiring[i] = 0;
+								return
+							}
+
+							else{
+				*/
+								newBullet(Enemy[i],Enemy[i+1],i);
+							//}
+
+						}// end long if-statement
+
+						if(bulletLoc[i] >= 597 || bulletLoc[i] <= 0 || bulletLoc[i+1] >= 397 || bulletLoc[i+1] <= 0){
+							newBullet(Enemy[i],Enemy[i+1],i);
+						}//end if
+
+					}//end bounds check
+
+		 	}//end for
 
 	  }//end checkBulletWallCollision()
 
@@ -518,7 +659,18 @@ $(document).ready(function(){
 	  	for(i = 0; i <= bulletLoc.length-1; i+=2){
 		  	bulletLoc[i] += (Math.cos(bulletAngle[i]))*timescaleRate/2;
 	  		bulletLoc[i+1] += (Math.sin(bulletAngle[i]))*timescaleRate/2;
-	  	}
+
+				//create trace to follow bullet
+				localTrace = [bulletLoc[i],bulletLoc[i+1]];
+
+				for(j = 2; j<20; j+=2){
+					localTrace[j] = localTrace[j-2]-(Math.cos(bulletAngle[i])*timescaleRate/1.25);
+					localTrace[j+1] = localTrace[j-1]-(Math.sin(bulletAngle[i])*timescaleRate/1.25);
+				}
+
+				enemyBulletTrace.splice(i,2,localTrace,0);
+
+			}
 	  } //end updateBullet
 
 		function updateEnemy(){
@@ -536,14 +688,174 @@ $(document).ready(function(){
 				//console.log("Calculation: " + Math.atan((playerY-Enemy[i+1])/(playerX-Enemy[i]))*(180/Math.PI));
 				//console.log("Enemy X: " + Enemy[i] +" Enemy Y: " + Enemy[i+1]);
 
+					path = enemyPath(Enemy[i], Enemy[i+1], playerX, playerY);
+					collisionPoint = pathObsticalCollisionCheck(path);
 
-				Enemy[i] += (Math.cos(enemyAngle[i]))*timescaleRate/4;
-				Enemy[i+1] += (Math.sin(enemyAngle[i]))*timescaleRate/4;
+
+				//}
+				if(collisionPoint){
+
+					winningEdge = nearestEdge(collisionPoint, playerX, playerY, true);
+
+					if(winningEdge[0]<Enemy[i]){
+						enemyAngle.splice(i,2,Math.atan((winningEdge[1]-Enemy[i+1])/(winningEdge[0]-Enemy[i]))+Math.PI,0);
+					}
+					else{
+						enemyAngle.splice(i,2,Math.atan((winningEdge[1]-Enemy[i+1])/(winningEdge[0]-Enemy[i])),0);
+					}
+
+					collisionPoint = false;
+
+
+				}
+
+					Enemy[i] += (Math.cos(enemyAngle[i]))*timescaleRate/4;
+					Enemy[i+1] += (Math.sin(enemyAngle[i]))*timescaleRate/4;
+
 			}
+
 		} //end updateBullet
 
-	  //aharris simplegame.js library accelerometer snippet
+		//creates a boundary array for obstacles
+		function createBoundaries(startX, startY, width, height){
 
+		  Bounds.push(999,999);
+
+		  for(i=0; i<=width; i+=1){
+
+		    Bounds.push(startX+i,startY);
+
+		  }
+		  Bounds.push(999,999);
+		  for(i=0; i<=width; i+=1){
+		    Bounds.push(startX+i,startY+height);
+		  }
+		  Bounds.push(999,999);
+		  for(i=0; i<=height; i+=1){
+		    Bounds.push(startX+width,startY+i);
+		  }
+		  Bounds.push(999,999);
+		  for(i=0; i<=height; i+=1){
+		    Bounds.push(startX,startY+i);
+		  }
+
+			Bounds.push(999,999);
+
+			startX = startX -20;
+			startY = startY -20;
+			width = width + 18;
+			height = height + 18;
+
+			//create Enemy Bounds
+			EnemyBounds.push(999,999);
+			for(i=0; i<=width; i+=1){
+
+				EnemyBounds.push(startX+i,startY);
+
+			}
+			EnemyBounds.push(999,999);
+			for(i=0; i<=width; i+=1){
+				EnemyBounds.push(startX+i,startY+height);
+			}
+			EnemyBounds.push(999,999);
+			for(i=0; i<=height; i+=1){
+				EnemyBounds.push(startX+width,startY+i);
+			}
+			EnemyBounds.push(999,999);
+			for(i=0; i<=height; i+=1){
+				EnemyBounds.push(startX,startY+i);
+			}
+			EnemyBounds.push(999,999);
+		  //return boundary;
+		}
+
+		//returns array of enemy line of sight directly to player
+		function enemyPath(enemyX, enemyY, playerX, playerY){
+		  length = Math.sqrt(Math.pow(enemyX-playerX,2) + Math.pow(enemyY-playerY,2));
+
+		  if(playerX<enemyX){
+				pathAngle = Math.atan((playerY-enemyY)/(playerX-enemyX))+Math.PI;
+			}
+			else{
+				pathAngle = Math.atan((playerY-enemyY)/(playerX-enemyX));
+			}
+
+		  enemyPathArray = [enemyX,enemyY];
+
+		  for(j=0; j<=Math.floor(length)*2; j+=2){
+		    enemyPathArray.push(enemyPathArray[j]+Math.cos(pathAngle), enemyPathArray[j+1] +Math.sin(pathAngle));
+		  }
+
+			//enemyPathArray.splice(0,10);
+
+		  return enemyPathArray;
+		}
+
+		function pathObsticalCollisionCheck(pathArray){
+
+		  //console.log("*Bounds Array Length: " + boundsArray.length);
+		  //console.log("*Path Array Length: " + pathArray.length);
+
+		  for(r=0; r<=pathArray.length;r+=2){
+		    for(s=0; s<=EnemyBounds.length;s+=2){
+
+		      if(Math.round(pathArray[r]) == EnemyBounds[s] && Math.round(pathArray[r+1]) == EnemyBounds[s+1]){
+
+		        return s;
+
+		      }
+
+		    }
+
+		  }
+
+		  return false;
+
+		}
+
+		function nearestEdge(index, playerX, playerY, isEnemy){
+		  d = index;
+
+			checkArray = Bounds;
+
+			if(isEnemy){
+				checkArray = EnemyBounds;
+			}
+
+		  while(checkArray[d] != 999){
+		    d-=2;
+		    edge1 = [checkArray[d+2], checkArray[d+3]];
+
+		  }
+
+		  //console.log(edge1);
+
+		  d = index;
+
+		  while(checkArray[d] != 999){
+		    d+=2;
+		    edge2 = [checkArray[d-2], checkArray[d-1]];
+		  }
+
+		  //console.log(edge2);
+
+		  edge1Length = Math.sqrt(Math.pow(edge1[0]-playerX,2) + Math.pow(edge1[1]-playerY,2));
+		  //console.log("Edge1 Length: " + edge1Length);
+		  edge2Length = Math.sqrt(Math.pow(edge2[0]-playerX,2) + Math.pow(edge2[1]-playerY,2));
+		  //console.log("Edge2 Length: " + edge2Length);
+
+		  if(edge1Length<=edge2Length){
+		    //console.log("Edge1 wins: " + edge1)
+		    return edge1;
+		  }
+		  else{
+		  //  console.log("Edge2 wins: " + edge2)
+		    return edge2;
+		  }
+
+		}
+
+	  //aharris simplegame.js library accelerometer snippet
 
 	  function Accel(){
 		  //virtual accelerometer
@@ -602,6 +914,30 @@ $(document).ready(function(){
 		  this.getRotZ = function(){return rotZ;}
 
 	} // end class def
+
+	function createLevel(){
+
+		playerX = currentLevel[0][0];
+		playerY = currentLevel[0][1];
+
+		Enemy = [];
+
+		for(i=0; i<currentLevel[1].length; i+=2){
+			newBullet(currentLevel[1][i],currentLevel[1][i+1],Enemy.length);
+			Enemy.push(currentLevel[1][i],currentLevel[1][i+1]);
+			enemiesFiring.push(1,"placeholder");
+
+		}
+
+
+		for(b=0; b<currentLevel[2].length; b+=4){
+			createBoundaries(currentLevel[2][b],currentLevel[2][b+1],currentLevel[2][b+2],currentLevel[2][b+3]);
+		}
+
+		coinX = currentLevel[3][0];
+		coinY = currentLevel[3][1];
+
+	}
 
 	function endGame() {
 
